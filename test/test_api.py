20c9bba8e1d6a2b53c7faf79053baabcf064547a
culv <39304490+culv@users.noreply.github.com>
2023-10-24 19:39:35 -0400

    Initial API with unit test for root domain

---
+++ test/test_api.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+
+from main import app
+
+client = TestClient(app)
+
+def test_api_locally_get_root():
+    r = client.get("/")
+    assert r.status_code == 200 and r.json() == "Welcome to the US Census Salary Prediction API"
