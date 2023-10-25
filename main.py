7873390376b83cce01e6e692e373a9ac8d460c36
culv <39304490+culv@users.noreply.github.com>
2023-10-24 20:26:44 -0400

    Add Data class and placeholder /predict endpoint

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Data(BaseModel):
+    age: int
+    workclass: str
+    fnlgt: int
+    education: str
+    education_num: int
+    marital_status: str
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital_gain: int
+    capital_loss: int
+    hours_per_week: int
+    native_country: str
+
 
 app = FastAPI()
 
@@ -9,5 +28,5 @@ async def hello():
 
 
 @app.post("/predict")
-async def predict():
-    pass
+async def predict(data: Data):
+    return data
