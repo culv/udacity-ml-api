deb58dd8021cf7041411a7b04e494e2f02e556fc
culv <39304490+culv@users.noreply.github.com>
2023-10-25 21:01:41 -0400

    Add slice performance

--- ml/model.py
+++ ml/model.py
@@ -1,4 +1,5 @@
 import joblib
+
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from sklearn.ensemble import RandomForestClassifier
 
@@ -47,6 +48,32 @@ def compute_model_metrics(y, preds):
     recall = recall_score(y, preds, zero_division=1)
     return precision, recall, fbeta
 
+
+def compute_column_slice_performance(model, X, y, df, column):
+    # First make sure the dataframe index is a RangeIndex going from 0 to len(df)-1
+    df_ = df.copy()
+    df_.reset_index(drop=True, inplace=True)
+
+    # Next make sure X and df have the same length
+    if X.shape[0] != df_.shape[0]:
+        raise Exception(f"Expected processed data X and original dataframe df to have \
+the same length. Got {X.shape[0]=} and {df.shape[0]=}")
+
+    # Start by getting unique values for the column
+    slices = df_[column].unique().tolist()
+    column_slice_perf = {}
+    for slice_ in slices:
+        # Grab only the data for the desired slice
+        slice_index = df_[df_[column] == slice_].index
+        X_slice = X[slice_index, :]
+        y_slice = y[slice_index]
+
+        # Compute metrics on the slice
+        y_pred = model.predict(X_slice)
+        prec, recall, fbeta = compute_model_metrics(y_slice, y_pred)
+        column_slice_perf[slice_] = {"slice_size": X_slice.shape[0], "precision": prec, "recall": recall, "fbeta": fbeta}
+
+    return column_slice_perf
 
 def inference(model, X):
     """Run model inferences and return the predictions.
--- ml/train_model.py
+++ ml/train_model.py
@@ -1,18 +1,18 @@
 # Script to train machine learning model.
-import joblib
+import json
 from pathlib import Path
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 from data import process_data
-from model import train_model, compute_model_metrics, inference, save_model
+from model import train_model, compute_model_metrics, compute_column_slice_performance, inference, save_model, RANDOM_STATE
 
 data_path = Path(__file__).absolute().parent.parent / "data" / "census_CLEAN.csv"
 data = pd.read_csv(data_path)
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+train, test = train_test_split(data, test_size=0.20, random_state=RANDOM_STATE)
 
 cat_features = [
     "workclass",
@@ -47,8 +47,20 @@ X_test, y_test, _, _ = process_data(
 # Train a model
 model = train_model(X_train, y_train)
 y_pred = inference(model, X_test)
+
+# Compute model performance on the test set
 precision, recall, fbeta = compute_model_metrics(y_pred, y_test)
 print(f"{precision=}, {recall=}, {fbeta=}")
+
+# Compute slice performance for each category
+cat_perfs = {}
+for cat in cat_features:
+    cat_perf = compute_column_slice_performance(model, X_test, y_test, test, cat)
+    cat_perfs[cat] = cat_perf
+
+slice_perf_path = data_path = Path(__file__).absolute().parent / "slice_performance.txt"
+with open(slice_perf_path, "w") as f:
+    f.write(json.dumps(cat_perfs, indent=4))
 
 # Save the model
 model_path = Path(__file__).absolute().parent.parent / "model" / "model.pkl"
